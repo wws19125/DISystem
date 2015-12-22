39f9e71e0f8429ffc33d949119e11053e06909ac
wang <wws19125@126.com>
2015-12-22 23:09:05 +0800

    connect to mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var url = 'mongodb://localhost:27017/test';
+  MongoClient.connect(url,function(err,db){
+    assert.equal(null,err);
+    console.log('Connected correctly to server.');
+    /*
+    db.collection("t",function(err,collection){
+      collection.find({name:'wang'},function(err,data){
+        if(data)
+        {
+          console.log(data);
+        }
+      });
+    });
+    */
+    var cursor = db.collection('t').find();
+    cursor.each(function(err,doc){
+      assert.equal(err,null);
+      if(doc!=null)
+      {
+        console.dir(doc);
+        db.close();
+      }
+    });
+    //db.close();
+  });
+  res.render('index', { title: 'DISystem' });
 });
 
 module.exports = router;
