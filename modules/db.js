348fbf9940cd1be39a1f36a304901eea9aa3ddbc
wang <wws19125@126.com>
2016-07-27 09:50:33 +0800

    db

--- modules/db.js
+++ modules/db.js
@@ -1,5 +1,11 @@
 var setting = require("../setting");
-var DB = require("mongodb").Db;
-var Connection = require("mongodb");//.Connection;
-var Server = require("mongodb").Server;
-module.exports = new DB(setting.db,new Server(setting.host,setting.port,{}),{safe:true});
+//var DB = require("mongodb").Db;
+//var Connection = require("mongodb");//.Connection;
+//var Server = require("mongodb").Server;
+var DB = {
+  open:function(callback){
+    callback(false,DB.db);
+  },
+  db:""
+};
+module.exports = DB;//new DB(setting.db,new Server(setting.host,setting.port,{}),{safe:true});
